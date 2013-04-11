28c6ab9b72b911a4c7c23bec09efbffeab34bb68
Jorrit Boekel <jorrit@horatio.bahnhof.se>
2013-04-11 16:43:51 +0200

    Different urls

--- olstats/urls.py
+++ olstats/urls.py
@@ -10,19 +10,12 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     )
 
-urlpatterns += patterns('race.views',
-    url(r'^race/$', 'index'),
-    
-    # Examples:
-    # url(r'^$', 'olstats.views.home', name='home'),
-    # url(r'^olstats/', include('olstats.foo.urls')),
-)
-
 urlpatterns += patterns('graphs.views',
-        #url(r'^$', 'index'), # or login page?
-        url(r'^graph/$', 'home'),
-        url(r'^graph/(?P<race_id>\d+)/$', 'race'),
-        url(r'^graph/(?P<race_ids>(?:\d+/)+)integrate', 'multirace'), # FIXME how to pass list?
-        url(r'^graph/dates/(?P<fromdate>\d+)/(?P<todate>\d+)/$', 'period'),
+        url(r'^$', 'home'),
+        url(r'^race/(?P<race_id>\d+)/$', 'race'),
+        url(r'^userraces/$', 'userraces'),
+
+        #url(r'^graph/(?P<race_ids>(?:\d+/)+)integrate', 'multirace'), # FIXME how to pass list?
+        #url(r'^graph/dates/(?P<fromdate>\d+)/(?P<todate>\d+)/$', 'period'),
 
 )
