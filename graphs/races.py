9415c963e22be0f8191ff50558884ac88776665f
Jorrit Boekel <glormph@gmail.com>
2013-04-12 11:57:01 +0200

    Refactored latest race showing from views.

--- graphs/races.py
+++ graphs/races.py
@@ -9,9 +9,8 @@ class RaceList(object):
     def get_latest_races(self, amount):
         """Returns last x races of a competitor"""
         racelist = self.racelist.order_by('-classrace__startdate')
-        racelist = [(x.classrace.id, x.classrace.name) for x in \
-                        racelist][:amount]
-        return racelist
+        racelist = [Race( x.classrace ) for x in racelist]
+        return racelist[:amount]
     
     def get_all_races_by_year(self):
         self.allraces = {}
@@ -36,5 +35,5 @@ class Race(object):
                     classrace.name.encode('utf-8'))
         self.classname = classrace.classname
         self.date = classrace.startdate
-
+        self.cr_id = classrace.id
     
