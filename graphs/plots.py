e71aef2c0c52a56af01f4757d25b9304fa90ba2a
Jorrit Boekel <glormph@gmail.com>
2013-04-13 21:36:12 +0200

    Improved y tickmarks for time and show them

--- graphs/plots.py
+++ graphs/plots.py
@@ -19,11 +19,11 @@ class PlotSet(object):
                                         #eventorID=2664
     
         self.plots['splits'] = MultiplePointsPerPersonPlot('splittider',
-                    racedata.data, self.show_eids, 'splits', 'tid', 'kontroll' )
+                    racedata.data, self.show_eids, 'splits', None, 'tid', 'kontroll' )
         self.plots['bomsplits'] = MultiplePointsPerPersonPlot('bommar', 
-                racedata.data, self.show_eids, 'mistakes', 'tid', 'kontroll')
+                racedata.data, self.show_eids, 'mistakes', None, 'tid', 'kontroll')
         self.plots['legbehind'] = MultiplePointsPerPersonPlot('tidskillnader',
-                racedata.data, self.show_eids, 'legdiffs', 'tid', 'kontroll')
+                racedata.data, self.show_eids, 'legdiffs', None, 'tid', 'kontroll')
         self.plots['spread'] = SingePointsPerPersonPlot('spread', racedata.data,
                 self.show_eids, 'spread', 'result', 'procent', 'placering',
                 'nottime')
@@ -32,6 +32,16 @@ class PlotSet(object):
                 'placering')
 
 class BasePlot(object):
+    def __init__(self, name, racedata, eid, y, x=None, ylab=None, xlab=None,
+                y_units=None):
+        self.highlight = eid
+        self.name = name
+        self.points = []
+        self.xlab = xlab
+        self.ylab = ylab
+        self.y_units = y_units
+
+
     def base_html(self):
         return """
         <div class="graph" id="%s"><div class="graphtitle">%s</div></div>
@@ -51,11 +61,11 @@ self.ylab)
     def render_data(self):
         return """data[plotname] = [ %s
         ];
+    var ymax = %s
     var xmax = d3.max(data[plotname], function(datum) {return datum.x;});
-    var ymax = d3.max(data[plotname], function(datum) {return datum.y;});
     var x = d3.scale.linear().domain([0, xmax]).range([padding, width-padding]);
     var y = d3.scale.linear().domain([0, ymax]).range([padding, height-padding]);
-        """ % ',\n'.join(self.points)
+        """ % (',\n'.join(self.points), self.ymax)
     
     def render_points(self):
         return """
@@ -85,30 +95,38 @@ height-padding).attr("stroke", "black");
 
 // tickmarks, 10 for x, 5 for y
 var xincrement = Math.ceil(xmax / 10);
-var yincrement = Math.round(ymax *100 / 5)/100;
 
+// x ticks
 axisGroup.selectAll(".xTicks").data(d3.range(0, xmax,
 xincrement)).enter().append("line").attr("y1", height-padding).attr("y2",
 height-padding+5).attr("x1", function(datum){return x(datum);}).attr("x2",
 function(datum) {return x(datum);}).attr("stroke", "black");
-
-axisGroup.selectAll(".yTicks").data(d3.range(0, ymax,
-yincrement)).enter().append("line").attr("x1", padding).attr("x2",
-padding-5).attr("y1", function(datum){return y(datum);}).attr("y2",
-function(datum) {return y(datum);}).attr("stroke", "black");
-
-// tick nrs
 
 axisGroup.selectAll(".xTickNos").data(d3.range(0, xmax,
 xincrement)).enter().append("text").attr("y", height-padding+15).attr("x", function(datum){return x(datum);})
 .text(function(datum) {return datum;}).attr("fill", "black").attr("class",
 "xTickNos").attr("text-anchor", "middle");
 
-axisGroup.selectAll(".yTickNos").data(d3.range(0, ymax,
-yincrement)).enter().append("text").attr("x", padding-10).attr("y",
-function(datum){return height-y(datum);})
-.text(function(datum) {return datum;}).attr("fill", "black").attr("class",
-"xTickNos").attr("text-anchor", "end").attr("dy", 3);
+// y ticks
+axisGroup.selectAll(".yTicks")
+    .data(ticks).enter()
+    .append("line")
+    .attr("x1", padding)
+    .attr("x2", padding-5)
+    .attr("y1", function(d) {return height-y(d.tick);})
+    .attr("y2", function(d) {return height-y(d.tick);})
+    .attr("stroke", "black");
+
+axisGroup.selectAll(".yTickNos")
+    .data(ticks).enter()
+    .append("text")
+    .attr("x", padding-10)
+    .attr("y", function(d){return height-y(d.tick);})
+    .text(function(d) {return d.mark;})
+    .attr("fill", "black")
+    .attr("class", "xTickNos")
+    .attr("text-anchor", "end")
+    .attr("dy", 3);
 
 // axis labels
 // x axis
@@ -135,8 +153,8 @@ axisGroup.append("text")
         """
 
     def render_html(self):
-        self.set_ytickmarks()
         html = [self.base_html()]
+        html.append( self.set_ytickmarks() )
         html.append(self.render_data() )
         html.append(self.render_points() )
         html.append(self.render_plot() )
@@ -144,42 +162,45 @@ axisGroup.append("text")
         return '\n'.join(html)
     
     def set_ytickmarks(self):
-        if self.y_units == 'time':
+        """Method to determine how tickmarks should be distributed"""
+        if self.y_units == 'time': # time tickmarks with minutes
+            ymax_minutes = int(self.ymax)/60
+            if ymax_minutes > 3: # >3 minutes: show whole minutes on ticks
+                rest = ymax_minutes % 5
+                ymax_minutes += (5-rest)
+                self.ymax = ymax_minutes * 60
+            else: # show multiples of 10 seconds
+                rest = int(self.ymax) % 10
+                self.ymax = int(self.ymax) + (10-rest)
+            
             yinc = int(self.ymax)/5 # 5 tickmarks
             ticks = range(0, int(self.ymax), yinc)
             ticks += [ticks[-1]+yinc]
             ticks = [{'tick': x, 'mark': '%d:%02d' % (x/60, x%60)} for x in ticks]
-            print ticks
+            ticks = """ticks = {0};""".format(ticks)
+            ticks = ticks.replace("\'tick\'", 'tick').replace('\'mark\'',
+                            'mark').replace('\'', '\"')
+            return ticks
 
 
 class SingePointsPerPersonPlot(BasePlot):
-    def __init__(self, name, racedata, eid, y, x, ylab, xlab, y_units='time'):
-        self.name = name
-        self.points = []
-        self.highlight = eid
+    def __init__(self, name, racedata, eid, y, x=None, ylab=None, xlab=None, y_units='time'):
+        super(SingePointsPerPersonPlot, self).__init__( name, racedata, eid, y, x, ylab, xlab, y_units)
         for pid in racedata:
             self.points.append('{name: "%s", x: %s, y: %s, color: "%s", eid: "%s"}' \
                 % (racedata[pid]['name'], racedata[pid][x], racedata[pid][y],
                 "grey", str(pid)))
          
-        self.xlab = xlab
-        self.ylab = ylab
-        self.y_units = y_units
         self.ymax = math.ceil(max([racedata[x][y] for x in racedata]))
 
 
 class MultiplePointsPerPersonPlot(BasePlot):
-    def __init__(self, name, racedata, eid, ydata, ylab, xlab=None,
+    def __init__(self, name, racedata, eid, y, x=None, ylab=None, xlab=None,
             y_units='time'):
-        self.highlight = eid
-        self.name = name
-        self.points = []
+        super(MultiplePointsPerPersonPlot, self).__init__( name, racedata, eid, y, x, ylab, xlab, y_units)
         points = {}
-        self.xlab = xlab
-        self.ylab = ylab
-        self.y_units = y_units
         for pid in racedata:
-            for n,spl in enumerate(racedata[pid][ydata]):
+            for n,spl in enumerate(racedata[pid][y]):
                 if not n in points:
                     points[n] = []
                 points[n].append('{name: "%s", x: %s, y: %s, color: "%s", eid: "%s"}' \
@@ -187,7 +208,8 @@ class MultiplePointsPerPersonPlot(BasePlot):
         
         xpoints = sorted(points.keys())
         self.points = [x for n in xpoints for x in points[n]]
-        self.ymax = math.ceil(max([racedata[x][ydata][1] for x in racedata]))
+        ymaxlist = [z for x in racedata for z in racedata[x][y]]
+        self.ymax = math.ceil(max(ymaxlist))
 
     def render_points(self):
         return """
