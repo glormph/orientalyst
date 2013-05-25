54ceabc55dc4e17d02d2064921b591c7689bcb18
Jorrit Boekel <glormph@gmail.com>
2013-05-25 11:10:19 +0200

    Rewrote db updating script to use django models and be much faster than upserting every single entry

--- eventor/init_update.py
+++ eventor/init_update.py
@@ -5,46 +5,26 @@ from olstats import settings
 from django.core.management import setup_environ
 setup_environ(settings)
 from django.contrib.auth.models import User
-
-def initialize():
-    db = postgres.DatabaseSession('olstats', 'jorrit')
-    data = eventorobjects.EventorData()
-    data.initialize()
-    init_persons_db(db, data)
-    update_db_results(db, data)
-    db.close()
+from graphs.models import Person, Event, Classrace, PersonRun, Result, Si,Split 
 
 def update():
     data = eventorobjects.EventorData()
-    data.get_persons()
-    db = postgres.DatabaseSession()
-    new_members, old_members = update_db_persons(db, data)
+    data.competitors = data.getCompetitors()
+    old_members, new_members = update_db_persons(data)
+    for person in new_members[:1]:
+        resultxml = data.getResults(person)
+        data.parseResults(person, resultxml)
+    for person in old_members[:1]:
+        resultxml = data.getResults(person, days=7) 
+        data.parseResults(person, resultxml)
     
-    for person in new_members:
-        data.getResults(person)
-    for person in old_members:
-        data.getResults(person, days=7) 
+    data.finalize() # modifies classraces into a list instead of convolutd dict
     
-    update_db_results(db, data)
-    db.close()
-
-
-def createTables(db):
-    pass
-    # IS DONE IN DJANGO with SYNCDB
-
-def init_persons_db(db, data):
-    print 'Writing to persons DB'
-    for person in data.competitors:
-        # if necessary, create a user account
-        userid = create_user_account(person)
-        db.runwrite('insert', 'graphs_person', schema=None, eventor_id=person.eventorID,
-                firstname=person.firstname, lastname=person.lastname,
-                user_id=userid)
-        person.sqlid = db.fetchone('graphs_person', ['id'],
-        eventor_id=person.eventorID)[0]
-        db.runwrite('insert', 'graphs_si', person_id=person.sqlid, si=person.SInr) 
-        person.si_sqlid = db.fetchone('graphs_si', ['id'], id=person.sqlid)[0]
+    events = update_events(data.events)
+    update_classraces(events, data.classraces)
+    update_results(data.classraces)
+    update_splits()
+    update_personruns()
 
 
 def create_user_account(person):
@@ -54,7 +34,7 @@ def create_user_account(person):
     except User.DoesNotExist:
         pass
     else:
-        return existing_user.id
+        return existing_user
 
     print 'Creating user account'
     # empty mail people get an account too. Wont be mailing them though.
@@ -82,74 +62,229 @@ def create_user_account(person):
     user.first_name = person.firstname
     user.last_name = person.lastname
     user.save()
-    return user.id
+    return user
 
-def update_db_results(db, data):
-    print 'Updating results'
-    # update events and get sqlid for them
-    for event in data.events.values():
-        db.runwrite('upsert', 'graphs_event', pk_fields=['eventor_id'], name=event.name, startdate=event.startdate,
-                eventor_id=event.eventorID)
-        event.sqlid = db.fetchone('graphs_event', ['id'], eventor_id=event.eventorID)[0]
-    # update classraces and get their sqlids
-    for raceid in data.classraces:
-        for classrace in data.classraces[raceid].values():
-            db.runwrite('upsert', 'graphs_classrace', pk_fields=['name', 'event_id',
-            'classname'], event_id=classrace.event.sqlid,
-            startdate=classrace.date, classname=classrace.classname,
-            racetype=classrace.racetype,lightcondition=classrace.lightcondition,
-            name=classrace.name)
-            classrace.sqlid = db.fetchone('graphs_classrace', ['id'],
-            name=classrace.name, event_id=classrace.event.sqlid,
-            classname=classrace.classname)[0]
-            
-            for personid in classrace.results:
-                db.runwrite('upsert', 'graphs_result', pk_fields=['classrace_id',
-                'person_eventor_id'], classrace_id=classrace.sqlid,
-                person_eventor_id=personid,
-                firstname=classrace.results[personid]['firstname'],
-                lastname=classrace.results[personid]['lastname'],
-                position=classrace.results[personid]['position'],
-                time=classrace.results[personid]['time'],
-                status=classrace.results[personid]['status'],
-                diff=classrace.results[personid]['diff']
-                )
-                resultssqlid = db.fetchone('graphs_result', ['id'],
-                classrace_id=classrace.sqlid, person_eventor_id=personid)[0]
-                
-                for splitcc in classrace.results[personid]['splits']:
-                    db.runwrite('upsert', 'graphs_split', pk_fields=['result_id',
-                    'split_n'], result_id=resultssqlid,
-                    split_n=splitcc,
-                    splittime=classrace.results[personid]['splits'][splitcc])
+def get_lookup_by_type(d, k):
+    if type(d) == dict:
+        out = d[k]
+    else:
+        out = getattr(d, k)
+    return out
 
-    # write to who-runs-what table
-    for person in data.competitors:
-        for eventraceid in person.classraces:
-            for classname in person.classraces[eventraceid]:
-                db.runwrite('upsert', 'graphs_personrun', ['person_id', 'si_id',
-                'classrace_id'], person_id=person.sqlid, si_id=person.si_sqlid,
-                classrace_id=person.classraces[eventraceid][classname].sqlid)
+def update_db_entry(obj, data, objkeys, datakeys):
+    changed = False
+    for ok,dk in zip(objkeys, datakeys):
+        if getattr(obj, ok) != get_lookup_by_type(data, dk):
+            setattr(obj, ok, get_lookup_by_type(data, dk))
+            changed = True
+    if changed:
+        obj.save()
+
+def generate_db_entry(model, data, objkeys, datakeys):
+    obj = model()
+    for ok,dk in zip(objkeys, datakeys):
+        setattr(obj, ok, get_lookup_by_type(data, dk))
+    return obj
 
 
-def update_db_persons(db, data):
-    new_members = []
-    old_members = []
-    for person in data.competitors:
-        # update/insert person db and check if new members exist
-        up = db.runwrite('upsert', 'graphs_person', ['eventor_id', 'firstname', 'lastname'], \
-             [person.eventorID, person.firstname, person.familyname])
-        if up:
-            new_members.append(person)
-        else:
-            old_members.append(person)
-        
-        # also update SI table 
-        person.sqlid = db.fetchone('graphs_person', ['id'],
-        eventor_id=person.eventorID)[0]
-        db.runwrite(upsert, 'graphs_si', ['person_id', 'si'], [person.sqlid, person.SInr])
+def update_events(events):
+    # split in old/new events
+    old_events = Event.objects.filter(eventor_id__in=[int(x) for x in \
+                    events]) # events is a dict with eventorIDs as keys
+    print 'eventor ids for old events', [int(x) for x in events]
+    old_events_ids = [x.eventor_id for x in old_events]
+    new_eventdata = {x: events[x] for x in events if x not in \
+                old_events_ids }
     
-    return new_members, old_members
+    # update old events if necessary
+    for event in old_events:
+        evd = events[ event.eventor_id ]
+        update_db_entry(event, evd, 
+                        ['name', 'startdate', 'eventor_id'],
+                        ['name', 'startdate', 'eventorID'])
+    # the rest: insert and give foreign key
+    new_events = []
+    for edata in new_eventdata.values():
+        event = generate_db_entry(Event, edata, 
+                    ['name', 'startdate', 'eventor_id'],
+                    ['name', 'startdate', 'eventorID'])
+        event.save()
+        all_events.append(event)
+        edata.eventfkey = event
+    all_events.extend(list(old_events))
+    return all_events
+
+def update_classraces(events, classraces):
+    # get classraces in db
+    cr_indb = Classrace.objects.filter(event__in=[x for x in
+                events])
+    # first create a dict containing pks for lookup and a tuple of the object and a
+    # dict with relevant fields for classrace comparison
+    cr_indb_dict = { x.pk: ( x, {'name': x.name, 'event': x.event, 
+                    'classname': x.classname} ) for x in cr_indb }
+    # filter downloaded classraces with/without db entry
+    for cr in classraces:
+        cr_idfields = {'name': cr.name, 'event': cr.event.eventfkey, 'classname': cr.classname}
+        for pk in cr_indb_dict:
+            if cr_idfields == cr_indb_dict[pk][1]:
+                # update old classrace
+                cr.classrace_fkey = cr_indb_dict[pk][0]
+                update_db_entry(cr.classrace_fkey, cr,
+                        ['event', 'startdate', 'classname', 'racetype',
+                                            'lightcondition', 'name'],
+                        ['event', 'date', 'classname', 'racetype', 'lightcondition',
+                                            'name'])
+                break
+            # new classrace if loop falls through
+            classrace = generate_db_entry(Classrace, cr,
+                    ['event', 'startdate', 'classname', 'racetype',
+                                            'lightcondition', 'name'],
+                    ['eventfkey', 'date', 'classname', 'racetype',
+                                            'lightcondition', 'name'])
+            classrace.save()
+            cr.classrace_fkey = classrace
+
+def update_results(classraces):
+    # get old results from db and make lookup dict
+    oldresults = Result.objects.filter(classrace__in=[x.classrace_fkey for x \
+                                                    in classraces])
+    oldreslookup, newresults, newresultdata = {}, [], {}
+    for x in oldresults:
+        if x.classrace not in oldreslookup:
+            oldreslookup[x.classrace] = {}
+        oldreslookup[x.classrace][x.person_eventor_id] = x
+    # now filter old results
+    for cr in classraces:
+        for personid in cr.results:
+            cr.results[personid]['eventorID'] = personid
+            cr.results[personid]['cr'] = cr.classrace_fkey
+            try:
+                oldresult = oldreslookup[cr][personid]
+            except KeyError:
+                newresultdata.append(cr.results[personid])
+                newresults.append(generate_db_entry(Result,
+                                    cr.results[personid], 
+                    ['classrace', 'person_eventor_id', 'firstname', 'lastname',
+                                        'position', 'time', 'status', 'diff'],
+                    ['cr', 'eventorID', 'firstname', 'lastname', 'position', 'time',
+                                                            'status', 'diff']))
+            else:
+                cr.results[personid]['resultobj'] = oldresult
+                update_db_entry(oldresult, cr.results[personid],
+                    ['classrace', 'person_eventor_id', 'firstname', 'lastname',
+                                        'position', 'time', 'status', 'diff'],
+                    ['cr', 'eventorID', 'firstname', 'lastname',
+                                        'position', 'time', 'status', 'diff'])
+    # new results: bulk_create, then get result obj and attach to result datas
+    Result.objects.bulk_create(newresults) 
+    newres_cr = Result.objects.filter(classrace__in=[x.classrace \
+                for x in newresults])
+    newres_lookup = {}
+    for res in newres_cr:
+        if res.classrace not in newres_lookup:
+            newres_lookup[ res.classrace ] = {}
+        newres_lookup[ res.classrace ][ res.person_eventor_id ] = res
+    for cr in data.classraces:
+        for personid in cr.results:
+            if not 'resultobj' in cr.results[personid]:
+                try:
+                    cr.results[personid]['resultobj'] = \
+                            newres_lookup[cr.classrace_fkey][personid]
+                except KeyError:
+                    raise
+                    pass # FIXME what if not in dict? Lookup problem?
+
+def update_splits(): #FIXME
+    # old splits: update
+    splitsindb = Split.objects.filter(result__in=\
+      [x.results[y]['resultobj'] for x in data.classraces for y in x.results])
+    splitslookup, newsplits = {}, []
+    for sp in splitsindb:
+        if sp.result not in splitslookup:
+            splitslookup[sp.result] = {}
+        splitslookup[sp.result][sp.split_n] = sp
+    for cr in data.classraces:
+        for pid in cr.results:
+            for sp in cr.results[pid]['splits']:
+                sp['resultobj'] = cr.results[pid]['resultobj']
+                try:
+                    spobj = \
+                        splitslookup[cr.results[pid]['resultobj']]['split_n']
+                except KeyError:
+                    newsplits.append( generate_db_entry(Split, sp,
+                    ['result', 'split_n', 'splittime'],
+                    ['resultobj', 'split_n', 'time']))
+                else:
+                    update_db_entry(spobj, sp,
+                        ['result', 'split_n', 'splittime'],
+                        ['resultobj', 'split_n', 'time'])
+                
+    # new splits: bulk create
+    Split.objects.bulk_create(newsplits)
+    
+    # write to who-runs-what table
+    data.person_runs = []
+    for p in data.competitors:
+        for erid in p.classraces:
+            for cn in p.classraces[erid]:
+                data.person_run.append({
+                    'person': p,
+                    'classrace': p.classraces[erid][cn].classrace_fkey,
+                    'si' : p.si_fkey
+                    })
+    
+    oldprs = PersonRun.objects.filter(classrace__in = data.classraces)
+    oldprlookup = {}
+    newprs = []
+    for pr in oldprs:
+        if not pr.classrace in oldprs:
+            oldprs[pr.classrace] = {}
+        oldprs[pr.classrace][pr.person] = pr
+    for prun in data.person_runs:
+        try:
+            probj = oldprlookup[ prun['classrace'] ][ prun['person'] ]
+        except KeyError:
+            newprs.append(PersonRun(  person=person.person_fkey,
+                    si = person.si_fkey,
+                    classrace =
+                    person.classraces[er_id][cname].classrace_fkey))
+        else:
+            update_db_entry(probj, prun,
+                    ['si', 'person', 'classrace'],
+                    ['si', 'person', 'classrace'])
+    
+    PersonRun.objects.bulk_create(newprs)
+    # done
+
+def update_db_persons(data):
+    """Feed downloaded eventor data, db will be updated with persons."""
+    new_members, new_persons = [], []
+
+    old_persons = Person.objects.all().filter(eventor_id__in=[x.eventorID \
+                for x in data.competitors])
+    
+    old_members_eventor = [str(x.eventor_id) for x in old_persons]
+    old_members = [x for x in data.competitors if x.eventorID in
+                        old_members_eventor]
+    for competitor in data.competitors:
+        if competitor.eventorID not in old_members_eventor:
+            useraccount = create_user_account(competitor)
+            person = Person(eventor_id=competitor.eventorID,
+                    firstname=competitor.firstname, lastname=competitor.lastname,
+                    user=useraccount)
+            person.save() # no need for bulk insert, usually few persons
+            new_persons.append( person )
+            new_members.append( competitor )
+
+    for person in old_members:
+        pass
+        # FIXME upsert in case of name/email change?
+    for person, member in zip(new_persons, new_members):
+        si = Si(si=member.SInr, person=person)
+        si.save()
+    
+    return old_members, new_members
+
 
 if __name__ == '__main__':
     initialize()
