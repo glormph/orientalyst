e5f8e2ec5af77192bc81e3756a9e3afe93491130
Jorrit Boekel <glormph@gmail.com>
2013-07-03 13:06:46 +0200

    Changed variable name and fixed bugs

--- data_input/management/commands/fetchresults.py
+++ data_input/management/commands/fetchresults.py
@@ -16,7 +16,7 @@ class Command(BaseCommand):
         make_option('--onlyold', '-o', action='store_true', dest='onlyold'),)
 
     def handle(self, *args, **options):
-        self.data = data.EventorData()
+        self.eventordata = data.EventorData()
         
         # FIXME check if options are ok
         # set amount of past days to download results from
@@ -37,38 +37,39 @@ class Command(BaseCommand):
             # better if no new people are fetched when updating w period.
         
         self.stdout.write('Downloading competitors from eventor...')
-        self.data.get_competitors(options['competitor'])
+
+        self.eventordata.get_competitors(options['competitor'])
         # FIXME check if problems with competitor download
         # if none downloaded, stop here (wrong ev_id, connection problems)
         if options['onlyold'] is False:
             self.stdout.write('Updating person database...')
-            old_members, new_members = dbupdate.update_db_persons(data)
+            old_members, new_members = dbupdate.update_db_persons(self.eventordata)
             # FIXME new members and personid?
         else:
             self.stdout.write('Only old members, skipping person database update...')
-            old_members, new_members = dbupdate.get_old_members(data)[0], []
+            old_members, new_members = dbupdate.get_old_members(self.eventordata)[0], []
         
         self.stdout.write('Downloading results data from eventor (may take a'
                ' while)...')
-        self.data.get_results(new_members, events=options['events'],
+        self.eventordata.get_results(new_members, events=options['events'],
                                     period=newperiod)
-        self.data.get_results(old_members, event=options['events'],
+        self.eventordata.get_results(old_members, event=options['events'],
                                 period=oldperiod)
         dbupdate.password_reset_for_new_users(new_members)
         self.update_db()
     
 
     def update_db(self):
-        self.data.finalize() # modifies classraces into a list instead of convolutd dict
+        self.eventordata.finalize() # modifies classraces into a list instead of convolutd dict
         self.stdout.write('Updating database...')
         self.stdout.write('Events...')
-        events = dbupdate.update_events(self.data.events)
+        events = dbupdate.update_events(self.eventordata.events)
         self.stdout.write('Races...')
-        dbupdate.update_classraces(events, self.data.classraces)
+        dbupdate.update_classraces(events, self.eventordata.classraces)
         self.stdout.write('Results...')
-        dbupdate.update_results(self.data.classraces)
+        dbupdate.update_results(self.eventordata.classraces)
         self.stdout.write('Splits...')
-        dbupdate.update_splits(self.data.classraces)
+        dbupdate.update_splits(self.eventordata.classraces)
         self.stdout.write('PersonRuns...')
-        dbupdate.update_personruns(self.data)
+        dbupdate.update_personruns(self.eventordata)
         self.stdout.write('All done!')
