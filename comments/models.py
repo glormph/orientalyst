7eee6b781f86a59202135e93c3e269812541bf9a
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2014-07-13 22:07:31 +0200

    Started comments app

---
+++ comments/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from accounts.models import Person
+from graphs.models import Classrace
+
+
+class Comment(models.Model):
+    comment = models.TextField()
+    author = models.ForeignKey(Person)
+    classrace = models.ForeignKey(Classrace)
+    created = models.DateTimeField(auto_add_now=True)
