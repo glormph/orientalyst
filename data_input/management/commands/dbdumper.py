8dcab383d5f4cba83bd2a9ec2a50a10851ea0570
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2014-07-18 23:08:22 +0200

    Made dbdump into django command

--- data_input/management/commands/dbdumper.py
+++ data_input/management/commands/dbdumper.py
@@ -1,5 +1,6 @@
 import os
 from django.core import serializers
+from django.core.management.base import BaseCommand
 from accounts.models import Person, Si
 from graphs.models import Event, EventRace, Classrace, PersonRun, Result
 from graphs.models import Split
@@ -15,27 +16,30 @@ def run_dump(queryset, outdir, outfile):
     with open(os.path.join(outdir, outfile)) as fp:
         serializer.serialize(queryset, stream=fp)
 
-# get persons
-pk_persons = [121, 114]  # Jorrit, Ågren
-test_persons = Person.objects.filter(pk__in=pk_persons)
 
-# dump accounts
-run_dump(test_persons, dumpdir, 'accounts.persons.json')
-run_dump(Si.objects.filter(person__in=test_persons), dumpdir,
-         'account.si.json')
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        # get persons
+        pk_persons = [121, 114]  # Jorrit, Ågren
+        test_persons = Person.objects.filter(pk__in=pk_persons)
 
-# prepare graph data
-person_runs = PersonRun.objects.filter(person__in=test_persons)
-classraces = Classrace.objects.filter(personrun__in=person_runs)
-eventraces = EventRace.objects.filter(classrace__in=classraces)
-events = Event.objects.filter(eventrace__in=eventraces)
-results = Result.objects.filter(classrace__in=classraces)
-splits = Split.objects.filter(result__in=results)
+        # dump accounts
+        run_dump(test_persons, dumpdir, 'accounts.persons.json')
+        run_dump(Si.objects.filter(person__in=test_persons), dumpdir,
+                 'account.si.json')
 
-# dump graph data
-for qset, outfn in zip([person_runs, classraces, eventraces, events, results,
-                        splits],
-                       ['graphs.{0}.json'.format(x) for x in
-                        ['personrun', 'classrace', 'eventrace', 'event',
-                         'result', 'split']]):
-    run_dump(qset, dumpdir, outfn)
+        # prepare graph data
+        person_runs = PersonRun.objects.filter(person__in=test_persons)
+        classraces = Classrace.objects.filter(personrun__in=person_runs)
+        eventraces = EventRace.objects.filter(classrace__in=classraces)
+        events = Event.objects.filter(eventrace__in=eventraces)
+        results = Result.objects.filter(classrace__in=classraces)
+        splits = Split.objects.filter(result__in=results)
+
+        # dump graph data
+        for qset, outfn in zip([person_runs, classraces, eventraces, events,
+                                results, splits],
+                               ['graphs.{0}.json'.format(x) for x in
+                                ['personrun', 'classrace', 'eventrace',
+                                 'event', 'result', 'split']]):
+            run_dump(qset, dumpdir, outfn)
