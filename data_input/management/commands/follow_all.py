f9d33430deed54d8056c517868f2c96c8fcd0520
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2014-07-20 10:47:42 +0200

    Oopsbug

--- data_input/management/commands/follow_all.py
+++ data_input/management/commands/follow_all.py
@@ -5,11 +5,11 @@ Script to let all persons get eachother as Follower
 from followers.models import Following
 from accounts.models import Person
 
-
+allfollowers = []
 for person_that_follows in Person.objects.all():
     for person_to_follow in Person.objects.all():
         if not person_that_follows == person_to_follow:
-            allfollowers = Following(followed=person_to_follow,
-                                     follower=person_that_follows,
-                                     )
+            allfollowers.append(Following(followed=person_to_follow,
+                                follower=person_that_follows,
+                                          ))
 Following.objects.bulk_create(allfollowers)
